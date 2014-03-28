a0d7bf93607c6604e3cfbfa8f47f4ac638c6ea12
Waruni <wch@exilesoft.com>
2014-03-28 15:56:21 +0530

    first commit

---
+++ src/GildedRose.Console/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRose.Console
+{
+    public class Program
+    {
+        public IList<Item> Items;
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("OMGHAI!");
+
+            var app = new Program
+                {
+                    Items = GetInitialItems()
+                };
+
+            app.UpdateQuality();
+            System.Console.ReadKey();
+        }
+
+        public static List<Item> GetInitialItems()
+        {
+            return new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item {Name = "Backstage passes to a TAFKAL80ETC concert",SellIn = 15,Quality = 20},
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                };
+        }
+
+        public   void UpdateQuality()
+        {
+            foreach (var t in Items)
+            {
+                Update(t);
+            }
+        }
+
+        private static void Update(Item t)
+        {
+            switch (t.Name)
+            {
+                case "Elixir of the Mongoose":
+                case "+5 Dexterity Vest":
+                    t.Quality = t.Quality > 0 ? t.Quality - 1 : t.Quality;
+                    t.SellIn = t.SellIn - 1;
+                    break;
+                case "Aged Brie":
+                    t.Quality = t.Quality < 50 ? t.Quality + 1 : t.Quality;
+                    t.SellIn = t.SellIn - 1;
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    t.Quality = t.Quality;
+                    t.SellIn = t.SellIn;
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    t.Quality = CalculateQualityOfBackstagePass(t.SellIn, t.Quality);
+                    t.SellIn = t.SellIn - 1;
+                    break;
+
+                case "Conjured Mana Cake":
+                    t.Quality = t.Quality > 0 ? t.Quality - 2 : t.Quality;
+                    t.SellIn = t.SellIn - 1;
+                    break;
+            }
+        }
+
+        /*private static void OldCodeUpdate(Item t)
+        {
+            if (t.Name != "Aged Brie" && t.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (t.Quality > 0)
+                {
+                    if (t.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        t.Quality = t.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (t.Quality < 50)
+                {
+                    t.Quality = t.Quality + 1;
+
+                    if (t.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (t.SellIn < 11)
+                        {
+                            if (t.Quality < 50)
+                            {
+                                t.Quality = t.Quality + 1;
+                            }
+                        }
+
+                        if (t.SellIn < 6)
+                        {
+                            if (t.Quality < 50)
+                            {
+                                t.Quality = t.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (t.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                t.SellIn = t.SellIn - 1;
+            }
+
+            if (t.SellIn < 0)
+            {
+                if (t.Name != "Aged Brie")
+                {
+                    if (t.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (t.Quality > 0)
+                        {
+                            if (t.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                t.Quality = t.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        t.Quality = t.Quality - t.Quality;
+                    }
+                }
+                else
+                {
+                    if (t.Quality < 50)
+                    {
+                        t.Quality = t.Quality + 1;
+                    }
+                }
+            }            
+        } */
+
+        private static int CalculateQualityOfBackstagePass(int sellIn, int quality)
+        {
+            if (sellIn <= 0) return 0;
+            if (sellIn > 0) quality++;
+            if (sellIn > 0 && sellIn <= 10) quality++;
+            if (sellIn > 0 && sellIn <= 5) quality++;
+            return quality > 50 ? 50 : quality;
+        } 
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+ 
+    }
+
+}
---
+++ src/GildedRose.Tests/UpdateItemsTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using GildedRose.Console;
+using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class UpdateItemsTests
+    {
+        [Test]
+        public void when_the_sell_in_has_passed_quality_degrades_twice_as_fast()
+        {
+            var item = UpdateQuality(new Item { Name = "Conjured Mana Cake", SellIn = 0, Quality = 2 });
+            Assert.That(item.Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void when_the_quality_of_an_item_is_never_negative()
+        {
+            var item = UpdateQuality(new Item { Name = "Conjured Mana Cake", SellIn = 2, Quality = 0 });
+            Assert.That(item.Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Aged_Brie_should_increase_in_quality_the_older_it_gets()
+        {
+            var item = UpdateQuality(new Item {Name = "Aged Brie", Quality = 1, SellIn = 1});
+            Assert.That(item.Quality, Is.EqualTo(2));
+        }
+
+        // [Test]
+        //public void quality_should_never_more_than_fifty()
+        //{
+        //    var item = UpdateQuality(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 60, SellIn = 0 });
+        //    Assert.That(item.Quality, Is.EqualTo(50));
+        //}  
+ 
+
+        [Test]
+        public void Sulfuras_legendary_item_it_should_not_decrease_in_quality()
+        {
+            var item = UpdateQuality(new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 });
+            Assert.That(item.Quality, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Sulfuras_legendary_item_it_should_not_decrease_sell_in()
+        {
+            var item = UpdateQuality(new Item { Name = "Sulfuras, Hand of Ragnaros", Quality = 1, SellIn = 1 });
+            Assert.That(item.SellIn, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Backstage_passes_it_should_increase_in_quality()
+        {
+            var item = UpdateQuality(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 11 });
+            Assert.That(item.Quality, Is.EqualTo(2));
+        }
+
+        [TestCase(10)]
+        [TestCase(9)]
+        [TestCase(8)]
+        [TestCase(7)]
+        [TestCase(6)]
+        public void Backstage_passes_it_should_increase_quality_by_two_when_there_are_ten_or_less_days_left(int daysLeft)
+        {
+            var item = UpdateQuality(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = daysLeft });
+            Assert.That(item.Quality, Is.EqualTo(3));
+        }
+
+        [TestCase(5)]
+        [TestCase(4)]
+        [TestCase(3)]
+        [TestCase(2)]
+        [TestCase(1)]
+        public void Backstage_passes_it_should_increase_quality_by_three_when_there_are_five_days_left(int daysLeft)
+        {
+            var item = UpdateQuality(new Item {Name = "Backstage passes to a TAFKAL80ETC concert",Quality = 1,SellIn = daysLeft});
+            Assert.That(item.Quality, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Backstage_passes_it_should_drop_quality_to_zero_when_sell_in_has_passed()
+        {
+            var item = UpdateQuality(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 1, SellIn = 0 });
+            Assert.That(item.Quality, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Conjured_it_should_degrade_in_quality_twice_as_fast_as_normal_items()
+        {
+            var item = UpdateQuality(new Item { Name = "Conjured Mana Cake", Quality = 4, SellIn = 0 });
+            Assert.That(item.Quality, Is.EqualTo(2));
+        } 
+
+
+        public static Item UpdateQuality(Item item)
+        {
+            var program = new Program {Items = new List<Item> {item}};
+            program.UpdateQuality();
+
+            return item;
+        }
+    }
+}
+ 
